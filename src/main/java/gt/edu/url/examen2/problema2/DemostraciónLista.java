58b44d7f9cdfb9b8fd544f41c84af0bf7fb59c80
Ana Lucía Godinez <anagod98@outlook.com>
2017-10-30 18:58:20 -0600

    commits

---
+++ src/main/java/gt/edu/url/examen2/problema2/DemostraciónLista.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gt.edu.url.examen2.problema2;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+/**
+ *
+ * @author Ana Godínez
+ */
+public class DemostraciónLista<E> implements List<E>, DemoList{
+    
+   public ArrayList(int capacity){
+        this(CAPACITY);
+   }
+    
+    
+    return Problema2;
+}
